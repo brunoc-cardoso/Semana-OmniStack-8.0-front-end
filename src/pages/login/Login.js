0529f38320c4c8c68c73edb1ca6d9be108e7fbc3
bruno <brunocpessoal@gmail.com>
2019-11-05 15:31:37 +0000

    added route navigation and basic pages

---
+++ src/pages/login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './Login.css';
+import logo from '../../assets/logo.svg';
+
+export default function Login() {
+  return (
+    <div className="login-container">
+
+      <form>
+        <img src={logo} alt="logo tindev"/>
+        <input
+          placeholder="Digite o seu usuário no Github"
+        />
+        <button type="submit">Enviar</button>
+      </form>
+
+    </div>
+  );
+}
