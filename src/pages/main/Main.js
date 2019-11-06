e1e8dbeec8170eddd6335ff24fe84229ef762c5f
bruno <brunocpessoal@gmail.com>
2019-11-06 17:34:46 +0000

    stylized main page

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,9 +1,64 @@
 import React from 'react';
 
 import './Main.css';
+import logo from '../../assets/logo.svg';
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
 
-export default function Main() {
+export default function Main({ match }) {
   return (
-    <h1>Main</h1>
+    <div className="main-container">
+      <img src={logo} alt="Tindev logo" />
+      <ul>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/33553892?v=4" alt=""/>
+          <footer>
+            <strong>Bruno Cardoso</strong>
+            <p>Uma programador web</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"/>
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars2.githubusercontent.com/u/33553892?v=4" alt=""/>
+          <footer>
+            <strong>Bruno Cardoso</strong>
+            <p>Uma programador web</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"/>
+            </button>
+          </div>
+        </li>
+        <li>
+          <img src="https://avatars0.githubusercontent.com/u/4248081?v=4" alt=""/>
+          <footer>
+            <strong>Bruno Cardoso</strong>
+            <p>Uma programador web</p>
+          </footer>
+
+          <div className="buttons">
+            <button type="button">
+              <img src={dislike} alt="dislike"/>
+            </button>
+            <button type="button">
+              <img src={like} alt="like"/>
+            </button>
+          </div>
+        </li>
+      </ul>
+    </div>
   );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const api = axios.create({
-  baseUrl: 'http://localhost:3333',
+  baseURL: 'http://localhost:3333',
 });
 
 export default api;
